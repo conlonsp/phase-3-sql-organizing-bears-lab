65a2e0e73efe3696d7a72747f0d00f33675715a2
samuelconlon <conlonsp@yahoo.com>
2023-01-20 14:17:01 -0500

    commit tree 21748199728b3d60307f90e66c91f54f9e3e501b

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "M", "Brown", "Sweet", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, "F", "Black", "Rowdy", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "Black", "Sleepy", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 1, "M", "White", "Authoritative", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 3, "F", "Brown", "Relaxed", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 7, "M", "Green", "Moody", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 3, "F", "Brown", "Playful", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "White", "Quiet", 1)
